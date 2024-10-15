6df1edb116cfe5960d2e69a550330e27bfa65048
Shivansh Sharma <123052704+Shivansh7102003@users.noreply.github.com>
2024-10-15 22:01:21 +0530

    Create Day-2.py

---
+++ Day-2.py
@@ -0,0 +1,47 @@
+maxsize = 3
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def is_empty(self):
+        if len(self.queue) == 0:
+            print("queue is empty")
+        else:
+            return None
+    def is_full(self):
+        if len(self.queue)==maxsize:
+            print("queue is full")
+        else:
+            return None
+    def enque(self , item):
+        if len(self.queue)==maxsize:
+            print("queue is full", item, "cant be inserted")
+        else:
+            self.queue = self.queue + [item]
+    def display(self):
+        if self.is_empty():
+            print("queue is empty")
+        else:
+            print("the current queue is:" , self.queue)
+    def deque(self):
+        if len(self.queue) == 0:
+            print("queue is empty")
+        else:
+            self.queue = self.queue[1:]
+
+if"__name__" == "__main__":
+  queue = Queue()
+
+  queue.enque(5)
+  queue.enque(6)
+  queue.enque(7)
+
+  queue.display()
+
+  queue.enque(8)
+
+  queue.deque()
+  queue.display()
+
+
+
+
